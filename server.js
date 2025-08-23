372f2d53ed0999478c959d4752f79e4eb5482f75
Tomasz-Gziut <tomasz.g.direct@gmail.com>
2025-08-23 13:04:45 +0200

    /feature added github api integration

--- server.js
+++ server.js
@@ -7,6 +7,25 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get("/repos", async (req, res) => {
+  try {
+    const response = await fetch("https://api.github.com/orgs/30osob-studio/repos", {
+      headers: { "User-Agent": "node.js" }
+    });
+
+    if (!response.ok) {
+      return res.status(response.status).json({ error: "Błąd pobierania danych z GitHuba" });
+    }
+
+    const data = await response.json();
+    res.json(data);
+
+  } catch (error) {
+    console.error("Błąd:", error);
+    res.status(500).json({ error: "Wewnętrzny błąd serwera" });
+  }
+});
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
